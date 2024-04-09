a58ca7c69cc6393635a598942df6bd6a1b8bf6cc
Disego <disegorachelm@gmail.com>
2024-04-09 12:32:30 +0200

    final

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName') ; 
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,8 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.innerHTML = `
+      <h1>Certificate of Achievement</h1>
+      <p>this is to certify that</p>
       <h3>${studentName}</h3>
+      <p>has almost completed the</p>
+      <h4>${courseName}</h4>
+      <p>with legendary perseverance and world-class bad-assery for never giving up 🏆</p>
+      <img src="logo.png" alt="Certificate Image">
+      <p>${personalMessage}</p>
     `;
     
       //  Display the modal
@@ -39,8 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none';
     });
   });
   
